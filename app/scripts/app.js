b2fc3363f28461e3f7bf81c6ee6e524aa00128a9
Victoria Finney <vfinney2@gmail.com>
2017-07-19 18:27:10 -0400

    Update links with routing based on ui-sref and implement collection state

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,7 +14,11 @@
           .state('album', {
              url: '/album',
              templateUrl: '/templates/album.html'
-         });
+         })
+          .state('collection', {
+                 url: '/collection',
+                 templateUrl: '/templates/collection.html'
+                 });
 }
 angular
   .module('blocJams', ['ui.router']);
