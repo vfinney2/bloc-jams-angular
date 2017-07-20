32eb9467e085028f106ae53c1c827189ff6a66cf
Victoria Finney <vfinney2@gmail.com>
2017-07-20 13:13:18 -0400

    Add controllers

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,26 +1,27 @@
-(function() {
-  function config($stateProvider, $locationProvider) {
-    $locationProvider
-         .html5Mode({
-             enabled: true,
-             requireBase: false
-         });
-    
-    $stateProvider
-         .state('landing', {
-             url: '/',
-             templateUrl: '/templates/landing.html'
-         })
-          .state('album', {
-             url: '/album',
-             templateUrl: '/templates/album.html'
-         })
-          .state('collection', {
-                 url: '/collection',
-                 templateUrl: '/templates/collection.html'
-                 });
-}
-angular
-  .module('blocJams', ['ui.router'])
-  .config(config);
+(function(){
+    function config($stateProvider, $locationProvider){
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+            });
+        $stateProvider
+           .state('landing',{
+                url: '/',
+                controller: 'LandingCtrl as landing',
+                templateUrl: '/templates/landing.html'
+            })
+           .state('album',{
+                url: '/album',
+                templateUrl: '/templates/album.html'
+            })
+           .state('collection',{
+               url: '/collection',
+               controller: 'CollectionCtrl as collection',
+               templateUrl: '/templates/collection.html'
+           });
+    }
+    angular
+        .module('blocJams', ['ui.router'])
+        .config(config);
 })();
