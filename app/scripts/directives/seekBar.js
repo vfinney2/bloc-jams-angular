2f4d8ce077ccbdfc6c510d3cd106b1d8abfa6452
Victoria Finney <vfinney2@gmail.com>
2017-07-28 23:21:34 -0400

    Add the ability to move thumb

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -31,6 +31,10 @@ scope.fillStyle = function() {
   return {width: percentString()};
 };
            
+scope.thumbStyle = function() {
+  return {left: percentString()};
+};
+           
 scope.onClickSeekBar = function(event) {
     var percent = calculatePercent(seekBar, event);
     scope.value = percent * scope.max;
